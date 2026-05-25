7e57371ca76c4ffdf00a396da537b21e106041b0
simonpunk <simonpunk2016@gmail.com>
2026-05-26 01:18:41 +0800

    Apply latest susfs patches

--- kernel/policy/app_profile.c
+++ kernel/policy/app_profile.c
@@ -14,7 +14,6 @@
 #include "klog.h" // IWYU pragma: keep
 #include "selinux/selinux.h"
 #include "infra/su_mount_ns.h"
-#include "hook/tp_marker.h"
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 7, 0)
 static struct group_info root_groups = { .usage = REFCOUNT_INIT(2) };
@@ -63,7 +62,7 @@ void setup_groups(struct root_profile *profile, struct cred *cred)
 
 void seccomp_filter_release(struct task_struct *tsk);
 
-static void disable_seccomp(void)
+void disable_seccomp(void)
 {
     struct task_struct *fake;
 
@@ -106,8 +105,6 @@ int escape_with_root_profile(void)
 {
     int ret = 0;
     struct cred *cred;
-    struct task_struct *p = current;
-    struct task_struct *t;
     struct root_profile *profile = NULL;
     struct user_struct *new_user;
 
@@ -117,7 +114,7 @@ int escape_with_root_profile(void)
         return -ENOMEM;
     }
 
-    if (cred->euid.val == 0) {
+    if (susfs_is_current_ksu_domain()) {
         pr_warn("Already root, don't escape!\n");
         goto out_abort_creds;
     }
@@ -179,11 +176,8 @@ int escape_with_root_profile(void)
 
     commit_creds(cred);
 
-    disable_seccomp();
-
-    for_each_thread (p, t) {
-        ksu_set_task_tracepoint_flag(t);
-    }
+    if (likely(test_thread_flag(TIF_SECCOMP)))
+        disable_seccomp();
 
     setup_mount_ns(profile->namespaces);
     ksu_put_root_profile(profile);
@@ -196,14 +190,16 @@ out_abort_creds:
     return ret;
 }
 
-void escape_to_root_for_init(void)
+int escape_to_root_for_init(void)
 {
     struct cred *cred = prepare_creds();
     if (!cred) {
         pr_err("Failed to prepare init's creds!\n");
-        return;
+        return -EINVAL;
     }
 
     setup_selinux(KERNEL_SU_CONTEXT, cred);
     commit_creds(cred);
+
+    return 0;
 }
